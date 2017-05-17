c8527feb090cbbb5fedd9ed2e4b7c696640b5538
Alonso Quesada <aquesada@fusionhit.com>
2017-05-17 16:16:32 -0600

    Base Commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function () {
+    return [
+        { title: 'Javascript the Good Parts', pages: 101 },
+        { title: 'Tao Te Ching', pages: 15 },
+        { title: 'The Dark Tower', pages: 41 },
+        { title: 'Clean Code', pages: 2 },
+        { title: 'Overwatch', pages: 1 }
+    ]
+}
